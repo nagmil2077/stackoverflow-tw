16b7a05c5bebe29f9ac4774b59c73330610a3144
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2023-08-22 14:08:16 +0200

    Add SqlConnector class

---
+++ src/main/java/com/codecool/stackoverflowtw/service/SqlConnector.java
@@ -0,0 +1,34 @@
+package com.codecool.stackoverflowtw.service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SqlConnector {
+
+    private final String url;
+    private final String userName;
+    private final String password;
+    private final String dbName;
+
+    public SqlConnector() {
+        this.userName = "postgres";
+        this.password = "shinedown";
+        this.dbName = "stackoverflow";
+        this.url = "jdbc:postgresql://localhost:5432/" + dbName;
+    }
+
+    public Connection getConnection() {
+        Connection conn;
+        try {
+            conn = DriverManager.getConnection(url, userName, password);
+            System.out.println("Connection to Postgresql has been established.");
+            return conn;
+        } catch (SQLException ex) {
+            System.err.println("Could not create database connection.");
+            throw new RuntimeException(ex);
+        }
+    }
+
+
+}
