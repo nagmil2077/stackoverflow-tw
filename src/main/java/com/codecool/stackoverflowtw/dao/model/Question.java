94fb989d73c61499bb2d9d491f70cbd623db1589
Mark Veszelka <120094709+markveszelka@users.noreply.github.com>
2023-08-22 13:26:35 +0200

    Convert Question class to record

--- src/main/java/com/codecool/stackoverflowtw/dao/model/Question.java
+++ src/main/java/com/codecool/stackoverflowtw/dao/model/Question.java
@@ -2,34 +2,7 @@ package com.codecool.stackoverflowtw.dao.model;
 
 import java.time.LocalDate;
 
-public class Question {
-    private final int id;
-    private final String title;
-    private final String description;
-    private final LocalDate localDate;
-
-    public Question(int id, String title, String description, LocalDate localDate) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.localDate = localDate;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public LocalDate getLocalDate() {
-        return localDate;
-    }
+public record Question(int id, String title, String description, LocalDate localDate) {
 
     @Override
     public String toString() {
