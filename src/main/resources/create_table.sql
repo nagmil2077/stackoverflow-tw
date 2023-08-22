cc0c8bca33cada8dc3889523737f9122aae72a6d
Mark Veszelka <120094709+markveszelka@users.noreply.github.com>
2023-08-22 13:09:44 +0200

    Add sql scripts

---
+++ src/main/resources/create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS question
+(
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    description TEXT,
+    date_created TIMESTAMP
+);
---
+++ src/main/resources/insert_table.sql
@@ -0,0 +1,3 @@
+INSERT INTO question
+    (id, title, description, date_created)
+VALUES (1, 'Test1', 'Test1', current_timestamp);
