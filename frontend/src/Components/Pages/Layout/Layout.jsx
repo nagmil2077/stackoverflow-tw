95de0deac6024e1a7eadb8972d77bf2ed050db2c
Mark Veszelka <120094709+markveszelka@users.noreply.github.com>
2023-08-23 13:43:03 +0200

    Modify Layout to show create answer button

--- frontend/src/Components/Pages/Layout/Layout.jsx
+++ frontend/src/Components/Pages/Layout/Layout.jsx
@@ -1,23 +1,47 @@
-import { Outlet, Link } from "react-router-dom";
+import {Link, Outlet, useParams, useLocation} from "react-router-dom";
 
 import "./Layout.css";
 
-const Layout = () => (
-    <div className="Layout">
-        <nav>
-            <ul>
-                <li className="grow">
-                    <Link to="/">Questions</Link>
-                </li>
-                <li>
-                    <Link to="/create">
-                        <button type="button">Create Question</button>
-                    </Link>
-                </li>
-            </ul>
-        </nav>
-        <Outlet />
-    </div>
-);
+const Layout = () => {
+    const location = useLocation();
+    const params = useParams();
+
+    if (location.pathname === `/question/${params.id}`) {
+        return <div className="Layout">
+            <nav>
+                <ul>
+                    <li className="grow">
+                        <Link to="/">Questions</Link>
+                    </li>
+                    <li>
+                        <Link to="/create/answer">
+                            <button type="button">Create Answer</button>
+                        </Link>
+                        <Link to="/create">
+                            <button type="button">Create Question</button>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+            <Outlet/>
+        </div>
+    } else {
+        return <div className="Layout">
+            <nav>
+                <ul>
+                    <li className="grow">
+                        <Link to="/">Questions</Link>
+                    </li>
+                    <li>
+                        <Link to="/create">
+                            <button type="button">Create Question</button>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+            <Outlet/>
+        </div>
+    }
+};
 
 export default Layout;
