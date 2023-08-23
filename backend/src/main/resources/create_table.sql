433c465497b03687ce9c86ae56223d7c384d48f4
BerryBusiness <120187639+BerryBusiness@users.noreply.github.com>
2023-08-23 12:42:39 +0200

    add script for creating joint tables

--- backend/src/main/resources/create_table.sql
+++ backend/src/main/resources/create_table.sql
@@ -5,3 +5,11 @@ CREATE TABLE IF NOT EXISTS question
     description TEXT,
     date_created TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS answer
+(
+    answer_id SERIAL PRIMARY KEY,
+    answer TEXT,
+    id INT,
+    FOREIGN KEY (id) REFERENCES question (id)
+);
