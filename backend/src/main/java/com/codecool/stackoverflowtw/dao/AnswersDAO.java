3def6f74027357d836594bd4de9cb2b44ef64db5
BerryBusiness <120187639+BerryBusiness@users.noreply.github.com>
2023-08-23 13:43:07 +0200

    prepare to delete

---
+++ backend/src/main/java/com/codecool/stackoverflowtw/dao/AnswersDAO.java
@@ -0,0 +1,21 @@
+package com.codecool.stackoverflowtw.dao;
+
+import com.codecool.stackoverflowtw.dao.model.Question;
+
+import java.util.List;
+
+public interface AnswersDAO {
+    void sayHi();
+    // CREATE
+    void add(int id, String title, String description);
+    // READ
+    Question get(int id);
+    // READ ALL
+    List<Question> getAll();
+    // UPDATE
+    void update(int id, String title, String description);
+    // DELETE
+    boolean delete(int id);
+    // DELETE ALL
+    boolean deleteAll();
+}
---
+++ backend/src/main/java/com/codecool/stackoverflowtw/dao/AnswersDaoJdbc.java
@@ -0,0 +1,156 @@
+package com.codecool.stackoverflowtw.dao;
+
+import com.codecool.stackoverflowtw.dao.model.Answer;
+import com.codecool.stackoverflowtw.service.SqlConnector;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnswersDaoJdbc implements AnswersDAO{
+    private final SqlConnector sqlConnector;
+
+    public QuestionsDaoJdbc(SqlConnector sqlConnector) {
+        this.sqlConnector = sqlConnector;
+    }
+
+    @Override
+    public void sayHi() {
+        System.out.println("Hi DAO!");
+    }
+
+    @Override
+    public void add(int id, String title, String description) {
+        String sql = "INSERT INTO question(title, description, date_created) VALUES (?, ?, ?)";
+
+        try (Connection conn = sqlConnector.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, title);
+            pstmt.setString(2, description);
+
+            Timestamp timestamp = Timestamp.valueOf(LocalDateTime.now());
+            System.out.println("Generated Timestamp: " + timestamp);
+
+            pstmt.setTimestamp(3, timestamp);
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public Question get(int id) {
+        String sql = "SELECT id, title, description, date_created FROM question WHERE id = ?";
+
+        try (Connection conn = sqlConnector.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+
+                    Question question = new Question(
+                            rs.getInt("id"),
+                            rs.getString("title"),
+                            rs.getString("description"),
+                            rs.getTimestamp("date_created").toLocalDateTime());
+
+                    System.out.println(
+                            rs.getInt("id") + "\t" +
+                                    rs.getString("title") + "\t" +
+                                    rs.getString("description") + "\t" +
+                                    rs.getTimestamp("date_created"));
+
+                    return question;
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<Question> getAll() {
+        String sql = "SELECT id, title, description, date_created FROM question " +
+                "ORDER BY id";
+        List<Question> questions = new ArrayList<>();
+
+        try (Connection conn = sqlConnector.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + "\t" +
+                        rs.getString("title") + "\t" +
+                        rs.getString("description") + "\t" +
+                        rs.getTimestamp("date_created"));
+                questions.add(new Question(
+                        rs.getInt("id"),
+                        rs.getString("title"),
+                        rs.getString("description"),
+                        rs.getTimestamp("date_created").toLocalDateTime()));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return questions;
+    }
+
+    @Override
+    public void update(int id, String title, String description) {
+        String sql = "UPDATE question SET title = ?, description = ? WHERE id = ?";
+
+        try (Connection conn = sqlConnector.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, title);
+            pstmt.setString(2, description);
+            pstmt.setInt(3, id);
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    @Override
+    public boolean delete(int id) {
+        String sql = "DELETE FROM question WHERE id = ?";
+
+        try (Connection conn = sqlConnector.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean deleteAll() {
+        String sql = "DELETE FROM question";
+
+        try (Connection conn = sqlConnector.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+
+        return true;
+    }
+}
